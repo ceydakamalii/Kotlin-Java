9e78a2ab3351fd44b98db19739c119fc4814f243
Ceyda Kamalı <ceydakamali3@gmail.com>
2021-12-13 22:31:21 +0300

    count down timer

--- CountDownTimer/app/src/main/java/com/ceydakamali/countdowntimer/MainActivity.java
+++ CountDownTimer/app/src/main/java/com/ceydakamali/countdowntimer/MainActivity.java
@@ -3,12 +3,32 @@ package com.ceydakamali.countdowntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView);
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText("Left: "+ millisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast toast = Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG);
+                toast.show();
+                textView.setText("Finished!");
+            }
+        }.start();
     }
 }
